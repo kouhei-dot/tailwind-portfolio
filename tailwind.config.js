33a3d3b4f220a5ea1e79bffef982b38455f49810
k.ikeda <o8011389077@gmail.com>
2022-10-14 20:55:50 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['poppins', 'sans-serif'],
+      },
+      colors: {
+        base: '#050505',
+        'link-text': '#A3A3FF',
+        secondary: '#9191A4',
+        bagge: '#3F3F51',
+        inputBorder: '#565666',
+        input: '#2A2A33',
+      },
+    },
+  },
+  plugins: [],
+}
